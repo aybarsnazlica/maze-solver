8a49fda6dc31edfbd409b7d644604fce36821a39
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-04-26 23:50:45 +0900

    Add line and point

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from display import Window
+from display import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(100, 400), Point(300, 200))
+    line2 = Line(Point(50, 250), Point(150, 350))
+    win.draw_line(line1, "red")
+    win.draw_line(line2, "blue")
     win.wait_for_close()
 
 
