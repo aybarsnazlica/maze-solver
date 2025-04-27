b80acb9fc79664f55f200c690b4dfb90354fbbe7
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-04-27 12:58:21 +0900

    Add a line between two cells

--- cell.py
+++ cell.py
@@ -1,14 +1,18 @@
 from display import Line, Point
 
 
+def half_distance(x, y):
+    return abs(x - y) // 2
+
+
 class Cell:
     def __init__(
-        self,
-        window,
-        has_left_wall=True,
-        has_right_wall=True,
-        has_top_wall=True,
-        has_bottom_wall=True,
+            self,
+            window,
+            has_left_wall=True,
+            has_right_wall=True,
+            has_top_wall=True,
+            has_bottom_wall=True,
     ):
         self.has_left_wall = has_left_wall
         self.has_right_wall = has_right_wall
@@ -19,14 +23,22 @@ class Cell:
         self._x2 = None
         self._y1 = None
         self._y2 = None
+        self.center = None
 
         self._window = window
 
     def draw(self, point1, point2):
+        if self._window is None:
+            return
+
         self._x1 = point1.x
         self._x2 = point2.x
         self._y1 = point1.y
         self._y2 = point2.y
+        self.center = Point(
+            self._x1 + half_distance(self._x1, self._x2),
+            self._y1 + half_distance(self._y1, self._y2),
+        )
 
         lines_to_draw = []
         line_left = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
@@ -48,3 +60,13 @@ class Cell:
 
         for line in lines_to_draw:
             self._window.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        move = Line(self.center, to_cell.center)
+
+        if undo:
+            fill_color = "gray"
+        else:
+            fill_color = "red"
+
+        self._window.draw_line(move, fill_color=fill_color)
--- main.py
+++ main.py
@@ -11,6 +11,15 @@ def main():
     c2 = Cell(window, has_right_wall=False)
     c2.draw(Point(150, 150), Point(200, 200))
 
+    c3 = Cell(window, has_top_wall=False)
+    c3.draw(Point(250, 250), Point(300, 300))
+
+    c4 = Cell(window, has_bottom_wall=False)
+    c4.draw(Point(450, 450), Point(500, 500))
+
+    c1.draw_move(c2)
+    c3.draw_move(c4, undo=True)
+
     window.wait_for_close()
 
 
